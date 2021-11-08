9f9eaf977e060d7af1e572aaaf1e6ad3ea3af0ce
Jelly <61914473+reesypiece@users.noreply.github.com>
2021-11-08 12:01:00 -0500

    added telescope keybinds

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -13,6 +13,13 @@ map('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>')
 map('n', '<leader>r', '<Cmd>NvimTreeRefresh<CR>')
 map('n', '<leader>n', '<Cmd>NvimTreeFindFile<CR>')
 
--- Bufferline.nvim
+-- Bufferline 
 map('n', '<A-z>', '<Cmd>BufferLineCyclePrev<CR>')
 map('n', '<A-x>', '<Cmd>BufferLineCycleNext<CR>')
+
+-- telescope
+map('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true })
+map('n', '<leader>fF', ':Telescope file_browser<CR>', { noremap = true })
+map('n', '<leader>fw', ':Telescope live_grep<CR>', { noremap = true })
+map('n', '<leader>fg', ':Telescope git_commits<CR>', { noremap = true })
+map('n', '<leader>fG', ':Telescope git_branches<CR>', { noremap = true })
